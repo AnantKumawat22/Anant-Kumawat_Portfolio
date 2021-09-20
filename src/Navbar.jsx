b1821ad99a8ad6ba537bbde4fec70a2c4061ff5c
Anant Kumawat <anantkumawat22@gmail.com>
2021-09-21 00:09:33 +0530

    third commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
             <label htmlFor="check" className="checkbtn">
                 <i class="fas fa-bars menu_icon"></i>
             </label>
-                <Link className="head_name" to="/">Anant Kumawat</Link>
+                <Link className="head_name" to="/">PortFolio</Link>
                 <ul>
                     <li><Link to="/"><i class="fas fa-home"></i> <span> Home</span></Link></li>
                     <li><Link to="/projects"><i class="fas fa-lightbulb"></i> <span> Projects</span></Link></li>
